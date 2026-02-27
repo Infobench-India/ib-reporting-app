49463518814820fb02245beb23f86fe09128cf37
Infobench-India <ashutosh.infobench@gmail.com>
2026-02-27 07:48:26 +0530

    Your current version of Antigravity is out of date. Please visit https://antigravity.google/download to download and install the latest version.

--- application-activation-tool/get_machine_id.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-const os = require('os');
-const fs = require('fs');
-
-const rawId = os.hostname() + os.arch() + os.platform();
-const machineId = crypto.createHash('sha256').update(rawId).digest('hex');
-
-fs.writeFileSync('machine-id.txt', machineId);
-console.log('✅ Machine ID:', machineId);
